ba86d02dc67b08d95d877136ae1493711357dc3c
Alaine A <alaine.uy@gmail.com>
2016-06-24 12:57:35 +0800

    A few config changes

--- processmonitor-setup.sh
+++ processmonitor-setup.sh
@@ -36,7 +36,7 @@ echo '### install rvm, ruby, rails and bundler'
 echo '#################################################'
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
+source /usr/local/rvm/scripts/rvm
 rvm requirements
 rvm install 2.0.0
 rvm use 2.0.0 --default
